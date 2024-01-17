8c318bea41767175977b897a0a38d8d37ca4f028
Abdullah <abdullah.k9913@gmail.com>
2024-01-17 22:16:09 +0500

    JAVA PROGRAMS

---
+++ VARIABLES/variables.java
@@ -0,0 +1,21 @@
+public class variables
+{
+    public static void main(String[] args)
+    {
+        int x=100;
+        long y=223432423423L;
+        byte b=100;
+        String s="hello";
+        char ch='@';
+        boolean bo=true;
+        float f=3.14f;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(ch);
+        System.out.println(bo);
+        System.out.println(f);
+    }
+}
