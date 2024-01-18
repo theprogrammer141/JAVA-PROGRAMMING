27c26440a4d33855ba2cd561f59ed858743089c6
Abdullah <abdullah.k9913@gmail.com>
2024-01-18 10:24:03 +0500

    USER INPUT

---
+++ USER INPUT/program.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class program{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER YOUR NAME:");
+        String name=scanner.nextLine();
+        System.out.println("YOUR NAME IS:"+name);
+
+        System.out.println("ENTER YOUR AGE:");
+        int age=scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("YOUR AGE IS:"+age);
+
+        System.out.println("ENTER YOUR FAVOURITE FOOD:");
+        String food=scanner.nextLine();
+        System.out.println("YOUR FAVOURITE FOOD IS:"+food);
+        
+    }
+}
