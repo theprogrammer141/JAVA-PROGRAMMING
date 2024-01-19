607050e00dbd4ea12a791fe0aab10e5616cab8a6
Abdullah <abdullah.k9913@gmail.com>
2024-01-19 16:00:01 +0500

    ADDED

---
+++ WRAPPER CLASSES/src/MAIN.java
@@ -0,0 +1,16 @@
+public class MAIN {
+    public static void main(String[] args)
+    {
+        //WRAPPER CLASSES BEHAVE EXACTLY LIKE PRIMITVE DATA TYPES
+        Boolean a=true;
+        Integer i=123;
+        String s="Bro";
+        Double d=3.4543;
+        Character c='#';
+
+        if(a)
+        {
+            System.out.println("THIS IS TRUE");
+        }
+    }
+}
