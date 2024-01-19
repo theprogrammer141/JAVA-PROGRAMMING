0811294bab8c61c41f58ee424a66cbd554eaa50b
Abdullah <abdullah.k9913@gmail.com>
2024-01-19 16:37:59 +0500

    ADDED

---
+++ METHODS/src/MAIN.java
@@ -0,0 +1,23 @@
+public class MAIN {
+    public static void main(String[] args)
+    {
+//        String name="BRO";
+//        hello(name);
+
+        int x=4;
+        int y=3;
+
+        int z=add(x,y);
+        System.out.println("THE SUM OF X AND Y IS:"+z);
+    }
+//    static void hello(String name)
+//    {
+//        System.out.println("HELLO "+name);
+//    }
+
+    static int add(int x,int y)
+    {
+        int z=x+y;
+        return z;
+    }
+}
